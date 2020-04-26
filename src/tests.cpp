ca62ada6fea720aa93170b6ad783c3f98d290f73
donromanos <roman.granacher@gmail.com>
2020-04-26 20:30:51 +0200

    Testing some settings

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,10 +1,7 @@
 #include "challenge.hpp"
 #include "reference.hpp"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments" // Caused by typed test variadic macro
 #include "gtest/gtest.h"
-#pragma clang diagnostic pop
 
 template <typename T>
 class Some_typed_Test : public ::testing::Test
